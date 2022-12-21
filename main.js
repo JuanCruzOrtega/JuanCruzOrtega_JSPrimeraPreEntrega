40fccca90b9f2b7e72ae9f7e2ade20e20634a6c8
juan_ortega <juancrortega@gmail.com>
2022-12-21 15:45:27 -0300

    2do commit: agregando datos al envio

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let productoElegido = ""
 let cantidadElegida = 0
 let precio = 0
 let precioTotal = 0
-let compraMas = "false"
+let compraMas = false;
 
 alert("Ingrese la opción que desea comprar, sino ingrese 0")
 productoElegido = parseInt(prompt("1- hamburguesa con cheddar ($1300), 2- hamburguesa con bacon ($1500), 3- hamburguesa clasica ($1500)"));
@@ -12,35 +12,41 @@ const cantidad = (cant, precio) => {
 }
 
 const totalEnvio = () => {
-    if (precio >= 3000){
-        alert("El envío es gratis")
-    } else{
-        precio <= 2999
-        alert("El costo de envío es 400, el total es " + (precio + 400) )
+    let conEnvioADomicilio = false;
+    conEnvioADomicilio = confirm("Queres envío a domicilio?")
+
+
+    if (conEnvioADomicilio && precio >= 3000) {
+        alert("El envío es gratis, el total es $" + (precio))
+    } else if (conEnvioADomicilio && precio < 3000) {
+        alert("El costo de envío es 400, el total es $" + (precio + 400))
+    } else {
+        alert("Podes pasarlo a retirarlo. El pedido estara listo en aproximadamente 30 minutos")
     }
 }
 
 
-while (productoElegido != "0") {
-    switch (productoElegido) {
-        case 1:
-            cantidadElegida = parseInt(prompt("Elegiste hamburguesa con cheddar. ¿Cuantas queres llevar?"))
-            precio += cantidad(cantidadElegida, 1300)
-            break;
-        case 2:
-            cantidadElegida = parseInt(prompt("Elegiste hamburguesa clasica. ¿Cuantas queres llevar?"))
-            precio += cantidad(cantidadElegida, 1500)
-            break;
-        case 3:
-            cantidadElegida = parseInt(prompt("Elegiste hamburguesa clasica. ¿Cuantas queres llevar?"))
-            precio += cantidad(cantidadElegida, 1500)
-            break;
-        default:
-            break;
-    }
-    productoElegido = parseInt(prompt("1- hamburguesa con cheddar ($1300), 2- hamburguesa con bacon ($1500), 3- hamburguesa clasica ($1500). Para salir ingresa 0"));
-}
 
-//alert("El total de la compra es " + precioTotal)
+    while (productoElegido != "0") {
+        switch (productoElegido) {
+            case 1:
+                cantidadElegida = parseInt(prompt("Elegiste hamburguesa con cheddar. ¿Cuantas queres llevar?"))
+                precio += cantidad(cantidadElegida, 1300)
+                break;
+            case 2:
+                cantidadElegida = parseInt(prompt("Elegiste hamburguesa clasica. ¿Cuantas queres llevar?"))
+                precio += cantidad(cantidadElegida, 1500)
+                break;
+            case 3:
+                cantidadElegida = parseInt(prompt("Elegiste hamburguesa clasica. ¿Cuantas queres llevar?"))
+                precio += cantidad(cantidadElegida, 1500)
+                break;
+            default:
+                productoElegido = 0;
+                cantidadElegida = 0;
+                break;
+        }
+        productoElegido = parseInt(prompt("1- hamburguesa con cheddar ($1300), 2- hamburguesa con bacon ($1500), 3- hamburguesa clasica ($1500). Para salir ingresa 0"));
+    }
 
 precioTotal = totalEnvio()
