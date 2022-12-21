7c479749fe96f2f2e02635fbb465806c2363fcc4
juan_ortega <juancrortega@gmail.com>
2022-12-20 22:27:26 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,88 @@
+// // // Solicitar nombre y apellido
+
+// // /*let nombreIngresado = prompt("Ingrese su nombre");
+// // let apellidoIngresado = prompt("Ingrese su apellido");
+// // const resultado = nombreIngresado + " " + apellidoIngresado;
+
+// // alert("Hola " + " " + resultado + " " + "bienvenido a O´Burgers")
+
+
+// // // Solicitar número
+
+// // let numeroA = Number(prompt("Ingrese un número"));
+// // const numeroB = Number(prompt("Ingrese otro número"));
+// // const resultado2 = numeroA + numeroB;
+
+// // console.log(resultado2)
+
+// // alert("Muy bien, el resultado es" + " " + resultado2)
+// // */
+
+
+
+// // let number = Number(prompt("Ingrese un número mayor a 1000"))
+
+// // if (number > 1000) {
+// //     // alert("CORRECTO, ES MAYOR A 1000")
+// // } else {
+// //     alert("Ponga bien el número")
+// // }
+
+// // //let number = Number(prompt("Ingresa un número"))
+
+// // //if ((number >= 10) && (number <= 50)) {
+// //     //alert("Tu numero está entre 10 y 50, es corecto.")
+// // //} else{
+// //     //alert("Tu numero es incorrecto")
+// // //} 
+
+
+// function calcularOperacion(primerNumero, segundoNumero, operacion) {
+//     switch (operacion) {
+//         case '+':
+//             return primerNumero + segundoNumero;
+//         case '-':
+//             return primerNumero - segundoNumero;
+//         case '*':
+//             return primerNumero * segundoNumero;
+//         case '/':
+//             return primerNumero / segundoNumero;
+//         default:
+//             return 0;
+//     }
+// };
+
+// let numeroA = Number(prompt("Ingrese su primer número"))
+// let numeroB = Number(prompt("Ingrese el segundo número"))
+// let operador = prompt("Ingrese si quiere sumar, restar, dividir o multiplicar")
+
+// let resultado = calcularOperacion(numeroA, numeroB, operador);
+
+// alert("El resultado de la operación es: "+ resultado)
+
+// console.log(resultado)
+//|| (producto !== "si") || (producto !== "Si") || (producto === "si") || (producto === "Si")
+
+//SIMULADOR INTERACTIVO
+
+let producto = "";
+let precio = 0;
+let cantidad = 0;
+let cantidadTotal = 0;
+let seguirComprando = false;
+let precioTotal = 0;
+
+
+
+    producto = prompt("¿Queres comprar una hamburguesa?");
+    cantidad = Number(prompt("Cuantas querrías comprar?"));
+
+    while (producto !== "SI" ) {
+        if (producto === "SI")  {
+         alert = ("Mirá nuestros combos") } else { alert = "Gracias por visitar nuestro sitio."}
+        cantidad = Number(prompt("Cuantas queres?"))
+
+    } 
+
+
+
